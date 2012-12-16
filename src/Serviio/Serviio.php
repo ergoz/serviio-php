643cace729a58f9fa5a13951b54a8f756dce506f
G. Arends <gdarends@gmail.com>
2012-12-16 16:14:55 +0100

    Revert "change property name"
    This reverts commit 9abd9c01803d98171dbfdb3ef92d725f2f760e9d.

--- src/Serviio/Serviio.php
+++ src/Serviio/Serviio.php
@@ -16,7 +16,7 @@ class Serviio
         'host'    => '127.0.0.1',
         'port'    => '23423',
         'secure'  => false,
-        'path' => '/rest/'
+        'service' => '/rest/'
     );
 
     /**
@@ -53,7 +53,7 @@ class Serviio
     private function setApi()
     {
         $api = sprintf(
-            '%s://' . $this->options['host'] . ':' . $this->options['port'] . $this->options['path'],
+            '%s://' . $this->options['host'] . ':' . $this->options['port'] . $this->options['service'],
             $this->options['secure'] ? 'https' : 'http'
         );
         $this->api = $api;
