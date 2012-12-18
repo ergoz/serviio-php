6ba29becadc51637240f318e847a89e74c3e6b72
G. Arends <gdarends@gmail.com>
2012-12-18 23:37:26 +0100

    fix typo

--- src/Serviio/CDS.php
+++ src/Serviio/CDS.php
@@ -4,7 +4,7 @@ namespace Serviio;
 
 class CDS extends Serviio
 {
-    public function __construct(array $uri = array())
+    public function __construct($uri = array())
     {
         $uri = array_merge($uri, array('service' => '/cds', 'port' => '23424'));
         parent::__construct($uri);
